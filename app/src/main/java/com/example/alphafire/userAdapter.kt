f090d583bd8aaa7963c62f1d69037fe58a85dea2
SoonYiLim <56568354+SoonYiLim@users.noreply.github.com>
2020-01-11 00:09:00 +0800

    sd

--- app/src/main/java/com/example/alphafire/userAdapter.kt
+++ app/src/main/java/com/example/alphafire/userAdapter.kt
@@ -2,14 +2,14 @@ package com.example.alphafire
 
 import android.app.AlertDialog
 import android.content.Context
-import android.content.DialogInterface
-import android.service.autofill.OnClickAction
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
 
 class userAdapter(val mctx: Context, val layoutResID: Int,val userList: List<user>)
     :ArrayAdapter<user>(mctx,layoutResID,userList){
@@ -21,27 +21,47 @@ class userAdapter(val mctx: Context, val layoutResID: Int,val userList: List<use
 
         val textViewLogin =  view.findViewById<TextView>(R.id.textViewLoginID)
         val textViewPW = view.findViewById<TextView>(R.id.textViewLoginPW)
+        val textViewTest = view.findViewById<TextView>(R.id.testTrue)
+
+
+        val tester:String = "asd"
+        var holder:Boolean = true
+
+        val holder1:String
+
 
         //here is the update de part
         val textViewLoginUpdate = view.findViewById<TextView>(R.id.Update)
 
         val user = userList[position]
 
+
         textViewLogin.text = user.loginId
 
         textViewPW.text = user.pw
 
+        if(user.pw.equals(tester))
+        {
+            holder = true
+            textViewTest.text = holder.toString()
+        }
+        else
+        {
+            holder = false
+            textViewTest.text = holder.toString()
+        }
+
         textViewLoginUpdate.setOnClickListener{
-            showUpdateDialog()
+            showUpdateDialog(user)
         }
 
         return view
 
     }
     //
-    fun showUpdateDialog(){
+    fun showUpdateDialog(user: user) {
         val builder = AlertDialog.Builder(mctx)
-
+        builder.setTitle("Update User")
         val inflater = LayoutInflater.from(mctx)
 
         val view = inflater.inflate(R.layout.updateuser,null)
@@ -49,10 +69,40 @@ class userAdapter(val mctx: Context, val layoutResID: Int,val userList: List<use
         val editText = view.findViewById<EditText>(R.id.UID)
         val editPw = view.findViewById<EditText>(R.id.pw)
 
+        editText.setText(user.loginId)
+        editPw.setText(user.pw)
+
         builder.setView(view)
 
         builder.setPositiveButton("Update"
         ) { p0, p1 ->
+            val dbuser = FirebaseDatabase.getInstance().getReference("users")
+
+            val  Uid = editText.text.toString().trim()
+            val  pass = editPw.text.toString().trim()
+
+
+            if(Uid.isEmpty())
+            {
+                editText.error = "Please enter ID"
+                editText.requestFocus()
+                return@setPositiveButton
+
+            }
+
+            if(pass.isEmpty())
+            {
+                editPw.error = "Please enter password"
+                editPw.requestFocus()
+                return@setPositiveButton
+            }
+
+            val user = user(user.id,Uid,pass)
+            dbuser.child(user.id).setValue(user)
+
+            Toast.makeText(mctx,"User updated",Toast.LENGTH_LONG).show()
+
+
 
         }
 
